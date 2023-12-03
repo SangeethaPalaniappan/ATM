464ef1eebc41a22199b80c6106dd11ad0006a440
Sangeetha <sangeethapa2002@gmail.com>
2023-12-03 21:35:50 +0530

    files details are changed

--- File(ATM) handling.py
+++ File(ATM) handling.py
@@ -1,29 +1,23 @@
+import sys
+
+# append a new directory to sys.path
+sys.path.append(r'C:\Users\sange\ATM')
+
+# now you can import your module
+import Bank_Account as bank
+
 arr = []
-g = open("python.txt")
+g = open("python2.txt")
 for text in g:
-    arr.append(text)
+    arr.append(text.split(","))
 g.close()
 print(arr)
-array = []
-for amount in arr:
-    array.append(amount.split(","))
-print(array)  
 
-for nums in range(len(array) - 2):
-    acc_no = int(array[nums][0])
-    acc_bal = int(array[nums][1])
-    print("acc_no   :", acc_no)
-    print("acc_bal  :", acc_bal)
-    
-atm_balance = 0
-atm_balance += int(array[len(array) - 1][0])
-hundrd_count = 0
-hundrd_count += int(array[len(array) - 1][1])
-two_hundrd_count = 0
-two_hundrd_count += int(array[len(array) - 1][2])
-five_hundrd_count = 0
-five_hundrd_count += int(array[len(array) - 1][3])
-print(hundrd_count)
-print(two_hundrd_count)
-print(five_hundrd_count)
-print(atm_balance)
+account_holder_array = []
+for i in range(len(arr) - 1):
+    account_holder = input("Account_holder_count : ")
+    account_holder_array.append(account_holder)
+print(account_holder_array)    
+for nums in range(len(arr) - 1):
+    account_holder_array[nums] = bank.Bank_account(arr[nums][0], arr[nums][1], arr[nums][3])
+
